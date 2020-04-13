8b0649ce8a2aa3293149abd8c1f02e6c229ea94a
Christer2222 <Christer__11@hotmail.com>
2020-04-13 04:08:16 +0200

    continued on the inventory

--- DC/Assets/_scripts/PlayerInventory.cs
+++ DC/Assets/_scripts/PlayerInventory.cs
@@ -8,9 +8,16 @@ public class PlayerInventory : MonoBehaviour
     public static PlayerInventory instance;
 
     public List<ItemQuantity> inventory = new List<ItemQuantity>();
+    private ItemQuantity selectedItem;
+
 
     public Transform dropList;
     private ItemInfoGameObject[] itemInfoGameObjects;
+
+    private GameObject itemDropPrefab;
+    private float itemDropPrefabHeight;
+
+    private float halfItemContextHeight;
 
     class ItemInfoGameObject
     {
@@ -28,6 +35,30 @@ public class PlayerInventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        itemDropPrefab = Resources.Load<GameObject>("Prefabs/InventoryItemEntry");
+        itemDropPrefabHeight = itemDropPrefab.GetComponent<RectTransform>().rect.height;
+
+        halfItemContextHeight = UIController.InventoryContextMenu.GetComponentInChildren<Image>().rectTransform.rect.height/2;
+
+        ItemQuantity[] debugItems =
+            {
+            new ItemQuantity() { count = 5, item = Items.apple },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.apple },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.orange },
+            new ItemQuantity() { count = 5, item = Items.banana }
+        };
+
+        inventory.AddRange(debugItems);
+
         instance = this;
         dropList = UIController.ItemDropListGameObject;
 
@@ -36,6 +67,60 @@ public class PlayerInventory : MonoBehaviour
         {
             var child = dropList.GetChild(i);
             itemInfoGameObjects[i] = new ItemInfoGameObject() { icon = child.GetComponentInChildren<Image>(), text = child.GetComponentInChildren<Text>() };
+        }
+
+        ClearInventory();
+        UpdateItemList();
+
+        UIController.InventoryUseButton.onClick.AddListener(() => print("used"));
+        UIController.InventoryEquipButton.onClick.AddListener(() => print("equiped"));
+        UIController.InventoryTossButton.onClick.AddListener(() => print("tossed " + UIController.InventoryTossInputField.text));
+        UIController.InventoryTossUp1Button.onClick.AddListener(() => ChangeTossNumber(+1)) ;
+        UIController.InventoryTossUp10Button.onClick.AddListener(   () => ChangeTossNumber(+10));
+        UIController.InventoryTossDown1Button.onClick.AddListener(  () => ChangeTossNumber(-1));
+        UIController.InventoryTossDown10Button.onClick.AddListener( () => ChangeTossNumber(-10));
+        
+    }
+
+    void ChangeTossNumber(int amount)
+    {
+        int newNum = int.TryParse(UIController.InventoryTossInputField.text, out newNum) ? newNum : 0;
+        newNum = Mathf.Clamp(newNum + amount, 0, 99);
+        UIController.InventoryTossInputField.text = newNum.ToString("00");
+    }
+
+    void ClearInventory()
+    {
+        for (int i = 0; i < UIController.InventoryItemContent.childCount; i++)
+        {
+            Destroy(UIController.InventoryItemContent.GetChild(i).gameObject);
+        }
+    }
+
+    void UpdateItemList()
+    {
+        UIController.InventoryItemContent.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (itemDropPrefabHeight + 20) * inventory.Count - 5);
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            GameObject go = Instantiate(itemDropPrefab, UIController.InventoryItemContent);
+            go.name = i + " " + inventory[i].item.name;
+            go.GetComponentInChildren<Image>().sprite = inventory[i].item.sprite;
+            go.GetComponentInChildren<Text>().text = $"{inventory[i].item.name} x{inventory[i].count}";
+            int index = i; //reference by value, otherwise all items would point to out of range
+            go.GetComponent<Button>().onClick.AddListener(() => {
+
+                selectedItem = inventory[index];
+                Vector3 pos = go.transform.position;
+                pos.x = 0;
+                //pos.y = Mathf.Clamp(pos.y,0 + halfItemContextHeight,1920 - halfItemContextHeight)/100;
+                Vector3 a = UIController.InventoryContextMenu.InverseTransformPoint(pos);
+                a.y = Mathf.Clamp(a.y, 0 + halfItemContextHeight, 1920 - halfItemContextHeight) / 100;
+                print(a);
+
+                UIController.InventoryContextMenu.position = a; //UIController.InventoryContextMenu.TransformPoint(pos);
+                print("clcik item " + selectedItem.item.name);
+                print("halfItemContextHeight: " + halfItemContextHeight);
+            });
         }
     }
 
--- DC/Assets/_scripts/UIController.cs
+++ DC/Assets/_scripts/UIController.cs
@@ -5,12 +5,27 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public static Transform ItemDropListGameObject { get; private set; }
+    public static RectTransform InventoryItemContent { get; private set; }
     
+    public static RectTransform InventoryContextMenu { get; private set; }
+    public static Button InventoryUseButton { get; private set; }
+    public static Button InventoryEquipButton { get; private set; }
+    public static Button InventoryTossButton { get; private set; }
+
+    public static Button InventoryTossUp1Button { get; private set; }
+    public static Button InventoryTossUp10Button { get; private set; }
+    public static Button InventoryTossDown1Button { get; private set; }
+    public static Button InventoryTossDown10Button { get; private set; }
+    public static InputField InventoryTossInputField { get; private set; }
+
+
+
     public static Slider HealthSlider { get; private set; }
     public static Slider ManaSlider { get; private set; }
 
     public static MinMax<Text> HealthTexts { get; private set; } //Text min, max;
     public static MinMax<Text> ManaTexts { get; private set; } //Text min, max;
+
 
     public class MinMax<T>
     {
@@ -30,13 +45,37 @@ public class UIController : MonoBehaviour
                 case "$ItemDropList":
                     ItemDropListGameObject = child;
                     break;
+                case "$ItemListContent":
+                    InventoryItemContent = child.GetComponent<RectTransform>();
+                    break;
+                case "$ContextMenu":
+                    InventoryContextMenu = child.GetComponent<RectTransform>();
+                    break;
+                case "$UseButton":
+                    InventoryUseButton = child.GetComponent<Button>();
+                    break;
+                case "$EquipButton":
+                    InventoryEquipButton = child.GetComponent<Button>();
+                    break;
+                case "$TossButton":
+                    InventoryTossButton = child.GetComponent<Button>();
+                    break;
+                case "$TossDown10Button":
+                    InventoryTossDown10Button = child.GetComponent<Button>();
+                    break;
+                case "$TossUp10Button":
+                    InventoryTossUp10Button = child.GetComponent<Button>();
+                    break;
+                case "$TossDown1Button":
+                    InventoryTossDown1Button = child.GetComponent<Button>();
+                    break;
+                case "$TossUp1Button":
+                    InventoryTossUp1Button = child.GetComponent<Button>();
+                    break;
+                case "$TossInputField":
+                    InventoryTossInputField = child.GetComponent<InputField>();
+                    break;
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
